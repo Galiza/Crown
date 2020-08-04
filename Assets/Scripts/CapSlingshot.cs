ea4b485dc852a19eec62946bdd9f44bc6af41784
Yuri Galiza <yuri.galiza@neon-free.ch>
2020-08-04 16:13:24 +0200

    Added extra assets for the field and added new scripts to manage players

---
+++ Assets/Scripts/CapSlingshot.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapSlingshot : MonoBehaviour
+{
+    private Vector3 initialpos;
+    private Vector3 finalPos;
+    private TrailRendererWith2DCollider trailManager;
+    private bool hasMoved = false;
+    private bool isMoving = false;
+    // private MeshCollider meshCollider;
+    private PolygonCollider2D polygonCollider;
+    private TrailRenderer tRendered;
+    private Rigidbody2D rigidBody;
+    private Vector3 distanceToGo;
+    private Vector3 onMouseUp;
+    private Vector3 onMouseDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        tRendered = GetComponent<TrailRenderer>();
+
+        trailManager = GetComponent<TrailRendererWith2DCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        initialpos = this.transform.position;
+        onMouseDown = Input.mousePosition;
+    }
+
+    void OnMouseDrag()
+    {
+
+    }
+
+    void OnMouseUp()
+    {
+        onMouseUp = Input.mousePosition;
+        distanceToGo = onMouseDown - onMouseUp;
+        rigidBody.AddForce(distanceToGo * 1f);
+        // if (trailManager.trail == null && rigidBody.velocity.magnitude.Equals(0)) {
+            trailManager.Build();
+        // }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.name.Contains(this.name))
+        {
+            Debug.Log("belong to same object");
+        }
+        else
+        {
+            Debug.Log("DOES NOT belong to same object");
+        }
+        // var rel = rigidBody.position - col.attachedRigidbody.position;
+
+        // rigidBody.AddForce(rel * 1f);
+
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private Dictionary<string, Vector3[]> tRPositions = new Dictionary<string, Vector3[]>();
+
+    private bool hasCrossedPaths = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetCapTrailRendererPosition(string tagName, Vector3[] position)
+    {
+        if (tRPositions.ContainsKey(tagName))
+        {
+            tRPositions[tagName] = position;
+            return;
+        }
+        tRPositions.Add(tagName, position);
+    }
+
+    public void HasCapCrossedPaths(string tagName, Vector3 currentPos)
+    {
+        foreach (KeyValuePair<string, Vector3[]> dictionary in tRPositions)
+        {
+            if (dictionary.Key != tagName)
+            {
+                foreach (Vector3 storedPos in dictionary.Value)
+                {
+                    if (storedPos.Equals(currentPos) && !storedPos.Equals(new Vector3()))
+                    {
+                        hasCrossedPaths = true;
+                        Debug.Log("Crossed paths");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
